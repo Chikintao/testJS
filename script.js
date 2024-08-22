ba928fdd9d7a267b8cfcba88db069329b928d33a
Maximus <111701085+Chikintao@users.noreply.github.com>
2024-08-22 21:28:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const _answer = confirm("тобі є 18?");
+const _yes = "Так";
+const _no = "Ні";
+
+if (_answer == true) {
+  console.log(`Mother say ${_yes}`);
+} else {
+  console.log(`Mother say ${_no}`);
+}
